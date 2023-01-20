2ee206e1128840b1aa9e4fef07edbd9fb10f5aef
Fanswers <alexis.larrode@gmail.com>
2023-01-20 16:34:17 +0100

    first api routes

---
+++ api.py
@@ -0,0 +1,86 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from database.send_to_database import get_database
+from bson.json_util import dumps
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+# Allow us to get json argument from method
+post_args = reqparse.RequestParser()
+post_args.add_argument("artist_id")
+post_args.add_argument("concerts")
+post_args.add_argument("spotify_id")
+post_args.add_argument("followers")
+post_args.add_argument("popularity")
+
+
+def get_collection():
+    client = get_database()
+    db = client['MusicProj']
+    collection = db["Artists"]
+
+    return collection
+
+# All artists
+class Artists(Resource):
+    def get(self):
+        collection = get_collection()
+
+        cursor = collection.find()
+        list_cur = list(cursor)
+
+        return list_cur
+
+    # Create One
+    def post(self):
+        args = post_args.parse_args()
+        collection = get_collection()
+
+        mydict = {"_id": args['artist_id'], "spotify_id": args['spotify_id'], "popularity": args['popularity']}
+
+        collection.insert_one(mydict)
+
+
+# Artist by name
+class Artist(Resource):
+    def get(self, artist_name):
+        collection = get_collection()
+
+        cursor = collection.find({"_id": artist_name})
+        list_cur = list(cursor)
+
+        return list_cur
+
+
+# Artist concerts
+class ArtistConcerts(Resource):
+    def get(self, artist_name):
+        collection = get_collection()
+
+        cursor = collection.find({"_id": artist_name})
+        concerts = cursor[0]["concerts"]
+
+        return concerts
+
+
+# Artist concert
+class ArtistConcert(Resource):
+    def get(self, artist_name, concert_date):
+        collection = get_collection()
+
+        cursor = collection.find({"_id": artist_name})
+        concert = cursor[0]["concerts"][concert_date]
+
+        return concert
+
+
+api.add_resource(Artists, '/')
+api.add_resource(Artist, '/<artist_name>')
+api.add_resource(ArtistConcerts, '/<artist_name>/concerts')
+api.add_resource(ArtistConcert, '/<artist_name>/concerts/<concert_date>')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
