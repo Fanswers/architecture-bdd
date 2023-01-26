a8c41c981e7afd7df058b92d5efb3bce49546bc7
Fanswers <alexis.larrode@gmail.com>
2023-01-26 16:50:53 +0100

    refactor(dockerisation): dockerize scraper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from scraper import scrap_artists_concerts, save_artists_concerts_json
-from spotify_api import get_token, get_spotify_info
-from database.send_to_database import get_database, insert_json, update_database
+from src.scraper import scrap_artists_concerts, save_artists_concerts_json
+from src.spotify_api import get_token, get_spotify_info
+from database.send_to_database import get_database, update_database
 
 # 13/01/2023 3749329
 if __name__ == "__main__":
