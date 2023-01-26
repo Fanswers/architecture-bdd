0b6e6a9950d5e2cd0d3f116d0c614f8b9e82d0c0
Fanswers <alexis.larrode@gmail.com>
2023-01-26 22:40:16 +0100

    refactor(dockerisation): add some logs

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from database.send_to_database import get_database, update_database
 # 13/01/2023 3749329
 if __name__ == "__main__":
     # Scrap concert site
+    print("scraping starting")
     artists_concerts_dict = scrap_artists_concerts()
+    print("scraping done")
 
     # Get spotify api token then get spotify data of each group
     spotify_auth = get_token()
@@ -16,9 +18,11 @@ if __name__ == "__main__":
 
     # Create a connexion to our database and send the json file
     client = get_database()
+    print("connected to database")
 
     # Init database
     # insert_json(client)
 
     # Update database
     update_database(client)
+    print("database updated")
